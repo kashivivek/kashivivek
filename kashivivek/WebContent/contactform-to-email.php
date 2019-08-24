37a87e5d18b5c3706dce2c7000296c83993a728d
Kashi Vivek Budda <kashivivek@gmail.com>
2019-08-24 14:25:15 -0700

    spam emails fix

--- kashivivek/WebContent/contactform-to-email.php
+++ kashivivek/WebContent/contactform-to-email.php
@@ -1,21 +1,24 @@
 <?php
+if (isset($_POST["websitekashivivek"]) && $_POST["websitekashivivek"] == "") {
 
-$name = $_POST['name'];
-$visitor_email = $_POST['email'];
-$message = $_POST['message'];
+    $name = $_POST['name'];
+    $visitor_email = $_POST['email'];
+    $message = $_POST['message'];
 
-$email_from = "$visitor_email";//<== update the email address
-$email_subject = "Contact us request from $name";
-$sucessMessage = "Your Request has been submitted";
-$email_body = "Hi Team, \n \nYou have received a new message from the user $name. \n".
-		"Here is the message:\n $message \n \n Thanks,\n Maintainence Team.";
+    $email_from = "$visitor_email"; // <== update the email address
+    $email_subject = "Contact us request from $name";
+    $sucessMessage = "Your Request has been submitted";
+    $email_body = "Hi Team, \n \nYou have received a new message from the user $name. \n" . "Here is the message:\n $message \n \n Thanks,\n KashiVivek website Maintainence Team.";
 
-$to = "kashivivek@gmail.com";//<== update the email address
-$headers = "From: $email_from \r\n";
-$headers .= "Reply-To: $visitor_email \r\n";
-//Send the email!
-mail($to,$email_subject,$email_body,$headers);
-//done. redirect to thank-you page.
-header('Location: index.html');
-
+    $to = "kashivivek@gmail.com"; // <== update the email address
+    $headers = "From: $email_from \r\n";
+    $headers .= "Reply-To: $visitor_email \r\n";
+    // Send the email!
+    mail($to, $email_subject, $email_body, $headers);
+    // done. redirect to thank-you page.
+    header('Location: index.html');
+} else {
+    http_response_code(400);
+    exit();
+}
 ?>
